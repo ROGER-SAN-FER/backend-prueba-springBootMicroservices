bd46aec9eadcaccf57d6fcf5317c8fb869324b83
ROGER-SAN-FER <roger190892@gmail.com>
2025-08-26 21:36:10 +0200

    Eliminar .idea del índice y usar .gitignore

--- pedidos-service/src/main/java/com/prueba/pedidosservice/dto/PedidoView.java
+++ pedidos-service/src/main/java/com/prueba/pedidosservice/dto/PedidoView.java
@@ -3,7 +3,6 @@ package com.prueba.pedidosservice.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.math.BigDecimal;
 
 // dto/PedidoView.java
