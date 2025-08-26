3fb3a00d1cde843975edac32b2e17bdc5a8fe940
ROGER-SAN-FER <roger190892@gmail.com>
2025-08-26 10:20:07 +0200

    Implementar auth-service con pom.xml basico

---
+++ api-gateway/src/main/java/com/prueba/apigateway/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+//package com.prueba.apigateway.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//
+//@Configuration
+//@EnableWebFluxSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+//        http
+//                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/actuator/**").permitAll()
+//                        .requestMatchers(HttpMethod.GET, "/productos/**").permitAll()   // ejemplo público
+//                        .anyRequest().authenticated()
+//                )
+//                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+//
+//        return http.build();
+//    }
+//}
