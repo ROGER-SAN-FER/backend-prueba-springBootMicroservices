e4ac09027450c49d6dbd3b1e3e1e2b551005e5ba
ROGER-SAN-FER <roger190892@gmail.com>
2025-08-26 20:39:43 +0200

    Add .idea and target to gitignore

--- productos-service/src/main/java/com/prueba/productosservice/controller/ProductosController.java
+++ productos-service/src/main/java/com/prueba/productosservice/controller/ProductosController.java
@@ -2,6 +2,7 @@ package com.prueba.productosservice.controller;
 
 import com.prueba.productosservice.dto.ProductDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 
+
 @RestController
 @RequestMapping("/productos")
 public class ProductosController {
 
     @GetMapping("/{id}")
     public ResponseEntity<ProductDto> getById(@PathVariable Long id) {
+        // Simulación (luego usaremos BD)
+        return ResponseEntity.ok(new ProductDto(id, "Teclado mecánico", new BigDecimal("59.90")));
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/{id}/admin")
+    public ResponseEntity<ProductDto> getByIdAdmin(@PathVariable Long id) {
         // Simulación (luego usaremos BD)
         return ResponseEntity.ok(new ProductDto(id, "Teclado mecánico", new BigDecimal("59.90")));
     }
