d9cd9af64fb454568aac0e926f59186567de7358
ROGER-SAN-FER <roger190892@gmail.com>
2025-08-26 20:55:01 +0200

    Stop tracking .idea completely

--- productos-service/src/main/java/com/prueba/productosservice/controller/ProductosController.java
+++ productos-service/src/main/java/com/prueba/productosservice/controller/ProductosController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
 
-
 @RestController
 @RequestMapping("/productos")
 public class ProductosController {
